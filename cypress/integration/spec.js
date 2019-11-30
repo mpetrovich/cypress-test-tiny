4f6f50be8307ea4cfc42f80389596b56df52d1be
Mike Petrovich <michael.c.petrovich@gmail.com>
2019-11-30 22:13:44 +0100

    Add reproducible test case

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
-describe('page', () => {
-  it('works', () => {})
+context('Bug test case', () => {
+    beforeEach(() => {
+        cy.visit('cypress/pages/index.html')
+    })
+
+    it('clear value of hidden input', () => {
+        cy.get('input').clear({ force: true }).should('have.value', '')
+    })
 })
